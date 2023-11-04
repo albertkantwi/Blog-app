92eccfc0cb1b52399ccc69b6b187f2d1dce8877d
unknown <albertkantwi@gmail.com>
2023-11-04 08:01:17 +0000

    Update post.rb file to fix post increament function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: :posts_counter
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: :post_counter
     has_many :likes, dependent: :destroy
     has_many :comments, dependent: :destroy
   
@@ -10,6 +10,6 @@ class Post < ApplicationRecord
     end
   
     def increment_user_posts_counter
-      author.increment!(:posts_counter)
+      author.increment!(:post_counter)
     end
   end

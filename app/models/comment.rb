1bcb0dec62ea01ddc6e1b15df86dfcb3a14bcee7
unknown <albertkantwi@gmail.com>
2023-11-04 13:52:37 +0000

    Add validations to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
-class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User'
-  belongs_to :post, counter_cache: :comments_counter, optional: true
-
-  after_save :update_comment_count
-
-  def update_comment_count
-    post.increment!(:comments_counter)
+  class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+  
+    after_create :update_comment_count
+  
+    def update_comment_count
+      post.increment!(:comments_counter)
+    end
   end
-end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,19 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: :posts_counter
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  belongs_to :author, class_name: 'User', counter_cache: :posts_counter
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  after_save :increment_user_posts_counter
+  before_create :increment_user_posts_counter
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
   def increment_user_posts_counter
-    author.increment!(:posts_counter)
+    author.increment!(:posts_counter) if author.present?
   end
 end

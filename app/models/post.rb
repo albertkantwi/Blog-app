0ffc687fbe12ec899ec8844d504b886bc2528112
unknown <albertkantwi@gmail.com>
2023-11-04 08:29:27 +0000

    Fix linters error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: :post_counter
-    has_many :likes, dependent: :destroy
-    has_many :comments, dependent: :destroy
-  
-    after_save :increment_user_posts_counter
-  
-    def recent_comments
-      comments.order(created_at: :desc).limit(5)
-    end
-  
-    def increment_user_posts_counter
-      author.increment!(:post_counter)
-    end
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: :post_counter
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  after_save :increment_user_posts_counter
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
+
+  def increment_user_posts_counter
+    author.increment!(:post_counter)
+  end
+end

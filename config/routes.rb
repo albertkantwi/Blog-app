79298d0dbe8fabdf429ef31377a69eab09ce75ab
unknown <albertkantwi@gmail.com>
2023-11-11 19:06:58 +0000

    Set up route

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-
-    root "users#index"
-    resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+      root "users#index"
+      resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
 end

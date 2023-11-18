456ce842d0a5cc94f299e7253624f3d77a846deb
unknown <albertkantwi@gmail.com>
2023-11-18 08:56:09 +0000

    Rearrange controllers#posts_controller.rb and layout#application.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,42 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show]
   before_action :initialize_like
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 6)
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
   def new
     @user = current_user
     @post = @user.posts.build
   end
+
   def create
     @user = current_user
     @post = @user.posts.build(post_params)
+
     if @post.save
       redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
     else
       render :new
     end
   end
+
   private
+
   def post_params
     params.require(:post).permit(:title, :text)
   end
+
   def find_post
     @post = Post.find(params[:id])
   end
+
   def initialize_like
     @like = Like.new
   end
